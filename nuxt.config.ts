b8abf05c7a3d786489e32d65e08ab1cd9f4d17e8
Kevin Cassere <kevin_cassere13@hotmail.com>
2023-12-12 21:13:22 -0300

    nuxt initial config(pinia, tailwindcss, nuxt-icon)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    'nuxt-icon',
+    '@pinia/nuxt',
+    '@pinia-plugin-persistedstate/nuxt',
+  ],
+  build: {
+    transpile: ['pinia-plugin-persistedstate'],
+  },
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 })
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="bg-red-500">
+        testing
+        <Icon name="mdi:close" size="100" />
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
